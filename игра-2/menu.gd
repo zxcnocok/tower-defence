a1aa3937c8b5ab02b767c7f105904c95e2d6344a
zxcnocok <d.natalia843@gmail.com>
2025-12-18 23:36:04 +0300

    полностью доделал механику поражения,улучшил механики мобов

--- игра-2/menu.gd
+++ игра-2/menu.gd
@@ -3,7 +3,7 @@ extends CanvasLayer
 
 
 func _on_start_pressed() -> void:
-	get_tree().change_scene_to_file("res://node_2d.tscn")
+	get_tree().change_scene_to_file("res://main.tscn")
 
 
 func _on_exit_pressed() -> void:
