3f6a1445f849af2ca6643033d36836959d666029
Jouons <Ximikat4805@gmail.com>
2025-12-19 15:10:45 +0300

    .

--- игра-2/arrow.gd
+++ игра-2/arrow.gd
@@ -1,24 +1,68 @@
 extends CharacterBody2D
 
-var target
-var Speed = 10
-var pathName = ""
-var arrowDamage
+var target = null  # Конкретный враг для преследования
+var speed = 300.0  # Скорость стрелы
+var damage = 5
+var alive_time = 3.0  # Время жизни стрелы (автоудаление)
+var current_time = 0.0
 
-func _physics_process(_delta):
-	var pathSpawnerNode = get_tree().get_root().get_node("Node2D2/Node2D2")
+func _ready():
+	# Автоматическое удаление через N секунд если не попала
+	set_physics_process(false)  # Ждем пока зададут цель
+
+func _physics_process(delta):
+	current_time += delta
 	
-	for i in pathSpawnerNode.get_child_count():
-		if pathSpawnerNode.get_child(1).name == pathName:
-			target = pathSpawnerNode.get_child(1).get_child(0).get_child(0).global_position
-			
+	# Удаляем если стрела слишком долго летит
+	if current_time >= alive_time:
+		queue_free()
+		return
 	
-	velocity = global_position.direction_to(target) *Speed
+	# Если цель уничтожена - удаляем стрелу
+	if target and not is_instance_valid(target):
+		queue_free()
+		return
 	
-	look_at(target)
+	# Летим к цели
+	if target and is_instance_valid(target):
+		var direction = global_position.direction_to(target.global_position)
+		velocity = direction * speed
+		
+		look_at(target.global_position)
+	else:
+		# Если нет цели - летим прямо
+		velocity = transform.x * speed
 	
 	move_and_slide()
-func _on_area_2d_body_entered(body):
-	if "CharacterBody2D" in body.name:
-			body.max_health -= arrowDamage
-			queue_free()
+	
+	# Проверяем столкновения
+	for i in get_slide_collision_count():
+		var collision = get_slide_collision(i)
+		_check_collision(collision.get_collider())
+
+func setup(new_target, new_damage):
+	# Инициализация стрелы
+	target = new_target
+	damage = new_damage
+	set_physics_process(true)
+	look_at(target.global_position)
+
+func _check_collision(body):
+	# Проверяем столкновение с врагом
+	if body and body.is_in_group("enemies"):
+		print("Стрела попала в: ", body.name)
+		
+		# Наносим урон
+		if body.has_method("take_damage"):
+			body.take_damage(damage)
+		elif body.has_method("hit"):
+			body.hit(damage)
+		elif "health" in body:
+			body.health -= damage
+		
+		# Удаляем стрелу
+		queue_free()
+
+# Альтернативно, можно использовать Area2D для обнаружения
+func _on_hitbox_body_entered(body):
+	_check_collision(body)
--- игра-2/idle_2_panel.gd
+++ игра-2/idle_2_panel.gd
@@ -1,36 +1,104 @@
 extends Panel
 
-
 @onready var tower = preload("res://idle2.tscn")
-var currTile
+var dragTower = null
+var isDragging = false
+var dragOffset = Vector2.ZERO
 
-func _on_gui_input(event):
-	var tempTower = tower.instantiate()
-	if event is InputEventMouseButton and event.button_mask == 1:
+func _ready():
+	# Включаем обработку ввода
+	mouse_filter = MOUSE_FILTER_PASS
+
+func _gui_input(event):
+	if event is InputEventMouseButton:
+		if event.button_index == MOUSE_BUTTON_LEFT:
+			if event.pressed:
+				# Начало перетаскивания
+				_on_mouse_down(event)
+			else:
+				# Завершение перетаскивания
+				_on_mouse_up(event)
+	
+	elif event is InputEventMouseMotion and isDragging:
+		# Перемещение башни
+		_on_mouse_move(event)
+
+func _on_mouse_down(event):
+	if dragTower == null:
+		# Создаем новую башню для перетаскивания
+		dragTower = tower.instantiate()
+		add_child(dragTower)
 		
-		add_child(tempTower)
+		# Устанавливаем начальную позицию
+		dragTower.position = get_local_mouse_position() - dragTower.size / 2
+		dragTower.scale = Vector2(0.32, 0.32)
 		
-		tempTower.process_mode = Node.PROCESS_MODE_DISABLED
+		# Сохраняем смещение курсора от центра башни
+		dragOffset = dragTower.position - get_local_mouse_position()
 		
-		tempTower.scale = Vector2(0.32,0.32)
+		isDragging = true
 		
-	elif event is InputEventMouseButton and event.button_mask == 1:
-		if get_child_count() > 1:
-			get_child(1).global_position = event.global_position
-		 	
-	elif event is InputEventMouseButton and event.button_mask == 0:
+		# Отключаем физику во время перетаскивания
+		dragTower.process_mode = Node.PROCESS_MODE_DISABLED
+		print("Начато перетаскивание")
+
+func _on_mouse_move(event):
+	if isDragging and dragTower:
+		# Обновляем позицию башни
+		dragTower.position = get_local_mouse_position() + dragOffset
+
+func _on_mouse_up(event):
+	if isDragging and dragTower:
+		var dropPosition = get_global_mouse_position()
 		
-		if event.global_position.x >= 510:
-			if get_child_count() > 1:
-				get_child(1).queue_free()
+		# Проверяем, можно ли разместить здесь башню
+		if _can_place_tower(dropPosition):
+			# Размещаем башню на игровом поле
+			var path = get_tree().root.get_node("Node2D2/Towers")
+			if path:
+				# Удаляем из панели
+				remove_child(dragTower)
+				
+				# Добавляем на игровое поле
+				path.add_child(dragTower)
+				dragTower.global_position = dropPosition
+				
+				# Включаем обработку
+				dragTower.process_mode = Node.PROCESS_MODE_INHERIT
+				
+				print("Башня размещена в позиции: ", dropPosition)
+			else:
+				print("Ошибка: нод для размещения не найден")
+				dragTower.queue_free()
 		else:
-			if get_child_count() > 1:
-				get_child(1).queue_free()
-			var path = get_tree().get_root().get_node("Node2D2/Towers")
-			
-			path.add_child(tempTower)
-			tempTower.global_position = event.global_position()
-			tempTower.get_node("Area").hide()
-	else:
-		if get_child_count() > 1:
-			get_child(1).queue_free()
+			# Нельзя разместить - удаляем
+			dragTower.queue_free()
+			print("Нельзя разместить здесь")
+		
+		# Сбрасываем состояние
+		dragTower = null
+		isDragging = false
+
+func _can_place_tower(position):
+	# Проверка на размещение
+	# 1. Не выходит за границы игровой области
+	if position.x < 510:  # Примерная граница
+		return true
+	return false
+
+# Альтернативная версия с использованием Area2D для проверки коллизий
+func _can_place_tower_v2(position):
+	var spaceState = get_world_2d().direct_space_state
+	var query = PhysicsRayQueryParameters2D.create(position, position, 1)
+	query.collide_with_areas = true
+	query.collide_with_bodies = true
+	query.exclude = []
+	
+	var result = spaceState.intersect_ray(query)
+	
+	# Если есть столкновение, нельзя разместить
+	if result:
+		print("Столкновение с: ", result.collider.name)
+		return false
+	
+	return true
